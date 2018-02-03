667c9eeb9ee8078306733d16ee6ac5f56e3a4659
Aditi Thakkar <aditimirani@gmail.com>
2018-02-03 17:11:17 +1100

    Change Log: 1. Initial commit with configuration of babel, webapck, eslint 2. Setup for webpack hotmodule replacement Changes By: Aditi Thakkar

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'React sample with .js file extension';
+ReactDOM.render(<div><h1>{title}</h1></div>,
+  document.getElementById('app')
+);
